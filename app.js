0723fd608ac26c4784fcaa91116b4279b55ea744
KareeMagdyy <karim67899@gmail.com>
2021-08-05 00:35:53 +0200

    JS Queries

---
+++ app.js
@@ -0,0 +1,44 @@
+const monsterHealthBar = document.querySelector('#monster-health');
+const playerHealthBar = document.querySelector('#player-health');
+const bonusLifeEl = document.querySelector('#bonus-life');
+
+const attackBtn = document.querySelector('#attack-btn');
+const strongAttackBtn = document.querySelector('#strong-attack-btn');
+const healthBtn = document.querySelector('#heal-btn');
+const logBtn = document.querySelector('#log-btn');
+
+function adjustHealthBars(maxLife) {
+  monsterHealthBar.max = maxLife;
+  monsterHealthBar.value = maxLife;
+  playerHealthBar.max = maxLife;
+  playerHealthBar.value = maxLife;
+}
+
+function dealMonsterDamage(damage) {
+  const dealtDamage = Math.random() * damage;
+  monsterHealthBar.value = +monsterHealthBar - dealtDamage;
+  return dealtDamage;
+}
+
+function dealPlayerDamage(damage) {
+  const dealtDamage = Math.random() * damage;
+  playerHealthBar.value = +playerHealthBar - dealtDamage;
+  return dealtDamage;
+}
+
+function increasePlayerHealth(healValue) {
+  playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function resetGame(value) {
+  playerHealthBar.value = value;
+  monsterHealthBar.value = value;
+}
+
+function removeBonusLife() {
+  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function setPlayerHealth(health) {
+  playerHealthBar.value = health;
+}
